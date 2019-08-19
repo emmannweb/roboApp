a6dbbb91fd640ff793d93aee76bd0b637a485d95
emmannweb <emmanuelnoc@gmail.com>
2019-08-18 22:27:44 -0300

    Updates

---
+++ precache-manifest.21e9d6873248bfcd03d04935409f5fa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e205e6fcf5562653a022694c8d4f65",
+    "url": "/roboApp/index.html"
+  },
+  {
+    "revision": "bcf96a91f490de3bdb2b",
+    "url": "/roboApp/static/css/main.a217740e.chunk.css"
+  },
+  {
+    "revision": "1bfc9120904935280a35",
+    "url": "/roboApp/static/js/2.33ce178a.chunk.js"
+  },
+  {
+    "revision": "bcf96a91f490de3bdb2b",
+    "url": "/roboApp/static/js/main.9578caba.chunk.js"
+  },
+  {
+    "revision": "c10e56e16e510bee0246",
+    "url": "/roboApp/static/js/runtime~main.c91768ff.js"
+  }
+]);
